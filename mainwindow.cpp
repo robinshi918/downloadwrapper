643ae359460c4a72e77c224f731f99476895141e
Robin Shi <robin.shi@eightdevelopment.com>
2021-03-24 15:56:40 +1300

    draft UI design

---
+++ mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QProcess>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    QProcess p;
+
+    delete ui;
+}
+

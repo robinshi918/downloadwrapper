ca40bdd99b62f02f5f6b6950c95c45350f5eadd3
Robin Shi <robin.shi@eightdevelopment.com>
2021-05-07 14:40:48 +1200

    show continuous output; inject system PATH environment

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -27,18 +27,24 @@ MainWindow::~MainWindow()
 
 void MainWindow::handleCancelButton()
 {
-    QString url = ui->urlEdit->text();
+
+}
+
+void MainWindow::showMessageBox(QString text)
+{
     QMessageBox msgBox;
-    msgBox.setText("url: " + url);
+    msgBox.setText(text);
     msgBox.exec();
 }
 
 void MainWindow::handleStartButton()
 {
     QString url = ui->urlEdit->text();
-    QMessageBox msgBox;
-    msgBox.setText("Start");
-    msgBox.exec();
+    if (url.isEmpty()) {
+        showMessageBox("download does not start because url is empty.");
+    } else {
+        downloadSingle(url);
+    }
 }
 
 void MainWindow::handleTypeSelected()
@@ -58,7 +64,9 @@ void MainWindow::init()
     ui->startEdit->setText("");
     ui->endEdit->setText("");
 
-    qInfo() << "MainWindow::init()";
+    ui->urlEdit->setText("https://www.youtube.com/watch?v=l11mUSu7aeA");
+
+//    qInfo() << "MainWindow::init()";
 }
 
 void MainWindow::downloadPlayList(QString &url, unsigned int startPos, unsigned int endPos)
@@ -68,8 +76,45 @@ void MainWindow::downloadPlayList(QString &url, unsigned int startPos, unsigned 
 
 void MainWindow::downloadSingle(QString &url)
 {
+    QStringList arguments{"-icw", "--extract-audio",  "--audio-format", "mp3", "--output","'/Users/robinshi/Desktop/QtTest/mp3/%(title)s.%(ext)s'",  url};
+//    qInfo() << arguments;
+
+    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
+        env.insert("PATH", env.value("PATH") + ":/usr/local/opt/openjdk/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/share/dotnet:~/.dotnet/tools:/Library/Apple/usr/bin:/Users/robinshi/Library/Android/sdk/platform-tools/");
+//        env.insert("PATH", env.value("PATH") + ":/opt/local/sbin");
+        p.setProcessEnvironment(env);
+
+
+
+    p.start("/usr/local/bin/youtube-dl", arguments);
+
+    connect(&p, SIGNAL(readyReadStandardOutput()),
+            this, SLOT(readSubProcess()));
+    connect(&p, SIGNAL(readyReadStandardError()),
+            this, SLOT(readSubProcess()));
+
 
 }
+
+void MainWindow::addToOutput(QString str)
+{
+    if (!str.isEmpty()) {
+        QString labelContent = ui->outputLabel->text();
+        ui->outputLabel->setText(labelContent + str);
+    }
+}
+
+void MainWindow::readSubProcess() {
+    QString stdout = p.readAllStandardOutput();
+    QString stderr = p.readAllStandardError();
+
+    qInfo() << stdout;
+    qInfo() << stderr;
+    addToOutput(stdout);
+    addToOutput(stderr);
+}
+
+
 
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QProcess>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -10,6 +11,7 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    QProcess p;
 
 public:
     MainWindow(QWidget *parent = nullptr);
@@ -27,5 +29,12 @@ private:
     void init();
     void downloadSingle(QString& url);
     void downloadPlayList(QString& url, unsigned int startPos, unsigned int endPos);
+    void showMessageBox(QString text);
+
+    void addToOutput(QString str);
+
+private slots:
+    void readSubProcess(void);
+
 };
 #endif // MAINWINDOW_H
